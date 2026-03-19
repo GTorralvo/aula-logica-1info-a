cfa3f6bd58fd98f1686b10b4adb0822e12061b2a
GTorralvo <gamestorralvo002@gmail.com>
2026-03-19 09:25:21 -0300

    Estamos terminando

---
+++ operadores-de-comparação.js
@@ -0,0 +1,5 @@
+// Um único = não é sianl de comparação, == igual === mais igual, ! não, != não igual (diferente) !==, mais diferente, <, >, >= maior, menor, maior ou igual, menor ou igual
+
+const minhavariavel = "valor" //atribuição
+
+console.log(5 == "5")
