fe81098f7b801c111632e2a0e09a24b288cb53a0
GTorralvo <gamestorralvo002@gmail.com>
2026-03-23 10:20:31 -0300

    Estamos no final

---
+++ estruturas-de-repeticao.js
@@ -0,0 +1,11 @@
+//wile, do wile, for
+
+let numero = 6
+let tabuada = 1
+let resultado = 0
+
+while(resultado <= numero * 10){
+    console.log (tabuada)
+    tabuada = tabuada + 1
+    resultado = tabuada * numero
+} 
